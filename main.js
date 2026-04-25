26d5e2113892c007b10c5abb7e9083d6d3c53e58
R-Islombek <islombekraximjanov97dev@gmail.com>
2026-04-25 15:20:33 +0500

    2

--- main.js
+++ main.js
@@ -7,3 +7,30 @@
 // }else{
 //     console.log(number - 1);
 // }
+
+// 2-Masala 
+
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (a > 0) {
+//     console.log(musbat + 1);
+// }
+// if(a < 0){
+//     console.log(manfiy - 1);
+// }
+// if(b > 0){
+//     console.log(musbat + 1);
+// }
+// if(c > 0 ){
+//     console.log(musbat + 1);
+// }
+// if(c < 0){
+//     console.log(manfiy - 1);
+// }
+
+// console.log("Musbat sonlar soni", musbat, "Manfiy sonlar soni", manfiy);
