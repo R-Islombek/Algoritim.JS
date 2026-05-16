17c526adb228273f753e20cfb5c4785ad287a052
R-Islombek <islombekraximjanov97dev@gmail.com>
2026-05-16 09:37:14 +0500

    1

---
+++ function.js
@@ -0,0 +1,14 @@
+// 1-Masala
+
+// let a = +prompt("a sonini kiriting");
+// let b = +prompt("b sonini kiriting");
+
+// function sum(a, b){
+
+//     let P = 2 * (a + b );
+//     let S = a * b
+//     return  ` Yuza ${S}, Pirametri ${P} `
+// }
+// console.log(sum(a, b));
+
+
