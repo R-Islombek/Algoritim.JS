011762d1f7b4cf3f6e85ef7928b8b0601321acf2
R-Islombek <islombekraximjanov97dev@gmail.com>
2026-05-16 15:24:34 +0500

    2

--- function.js
+++ function.js
@@ -13,14 +13,14 @@
 
 // 2-Masala;
 
-let a = prompt("a tomonini kiriting");
+// let a = prompt("a tomonini kiriting");
 
-function triangle(a){
-    let P = 3 * a;
-    let S = (Math.sqrt(3) / 4) * Math.pow(a, 2);
+// function triangle(a){
+//     let P = 3 * a;
+//     let S = (Math.sqrt(3) / 4) * Math.pow(a, 2);
 
-    return ` Yuzi ${S}, Pirametiri ${P} `
-}
-console.log(triangle(a));
+//     return ` Yuzi ${S}, Pirametiri ${P} `
+// }
+// console.log(triangle(a));
 
 
